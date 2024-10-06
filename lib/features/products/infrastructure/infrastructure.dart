7c32b23175f3bcfcae94c171f9f45bbd294c2be3
Alexhr <alex.hernandez24kb@gmail.com>
2024-10-05 23:04:46 -0600

    Riverpod - Product Repository Provider

---
+++ lib/features/products/infrastructure/infrastructure.dart
@@ -0,0 +1,3 @@
+export 'package:teslo_app/features/products/domain/datasources/products_datasource.dart';
+export 'package:teslo_app/features/products/domain/repositories/products_repository.dart';
+export 'package:teslo_app/features/products/infrastructure/mapers/product_maper.dart';
