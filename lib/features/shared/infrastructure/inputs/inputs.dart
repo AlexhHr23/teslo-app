2cb0227a9f43ce61a492e126a52621b4c3f32190
Alexhr <alex.hernandez24kb@gmail.com>
2024-10-08 21:24:13 -0600

    ProductForm

--- lib/features/shared/infrastructure/inputs/inputs.dart
+++ lib/features/shared/infrastructure/inputs/inputs.dart
@@ -1,2 +1,7 @@
+export 'package:teslo_app/features/shared/infrastructure/inputs/title.dart';
+export 'package:teslo_app/features/shared/infrastructure/inputs/price.dart';
+export 'package:teslo_app/features/shared/infrastructure/inputs/slug.dart';
+export 'package:teslo_app/features/shared/infrastructure/inputs/stockj.dart';
 export 'package:teslo_app/features/shared/infrastructure/inputs/email.dart';
 export 'package:teslo_app/features/shared/infrastructure/inputs/password.dart';
+
