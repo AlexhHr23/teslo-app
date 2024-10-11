eb8ce85c112aa70ebe174516e977bc4e44311ed1
Alexhr <alex.hernandez24kb@gmail.com>
2024-10-10 22:34:44 -0600

    subir imagen

--- lib/features/products/infrastructure/datasources/products_datasource_impl.dart
+++ lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -1,35 +1,78 @@
-
-
 import 'package:dio/dio.dart';
-import 'package:teslo_app/config/config.dart';
-import 'package:teslo_app/features/products/infrastructure/errors/product_errors.dart';
 import 'package:teslo_app/features/products/infrastructure/mapers/product_maper.dart';
 
-import '../../domain/domain.dart';
 
-class ProductsDatasourceImpl extends ProductsDatasource{
+
+import '../../../../config/config.dart';
+import '../../domain/domain.dart';
+import '../errors/product_errors.dart';
+
+
+
+class ProductsDatasourceImpl extends ProductsDatasource {
 
   late final Dio dio;
   final String accessToken;
 
   ProductsDatasourceImpl({
     required this.accessToken
-    }): dio = Dio(
-      BaseOptions(
-        baseUrl: Environment.apiUrl,
-        headers: {
-          'Authorization': 'Bearer $accessToken'
-        }
-      )
-    );
+  }) : dio = Dio(
+    BaseOptions(
+      baseUrl: Environment.apiUrl,
+      headers: {
+        'Authorization': 'Bearer $accessToken'
+      }
+    )
+  );
+
+
+  Future<String> _uploadFile( String path ) async {
+
+    try {
+
+      final fileName = path.split('/').last;
+      final FormData data = FormData.fromMap({
+        'file': MultipartFile.fromFileSync(path, filename: fileName)
+      });
+
+      final respose = await dio.post('/files/product', data: data );
+
+      return respose.data['image'];
+
+    } catch (e) {
+      throw Exception();
+    }
+
+
+  }
+
+
+  Future<List<String>> _uploadPhotos( List<String> photos ) async {
+    
+    final photosToUpload = photos.where((element) => element.contains('/') ).toList();
+    final photosToIgnore = photos.where((element) => !element.contains('/') ).toList();
+
+    //Todo: crear una serie de Futures de carga de imágenes
+    final List<Future<String>> uploadJob = photosToUpload.map(_uploadFile).toList();
+
+    final newImages = await Future.wait(uploadJob);
+    
+    return [...photosToIgnore, ...newImages ];
+  }
+
 
   @override
-  Future<Product> createUdateProduct(Map<String, dynamic> productLike) async{
+  Future<Product> createUdateProduct(Map<String, dynamic> productLike) async {
+    
     try {
+      
       final String? productId = productLike['id'];
-      final String method = (productId == null) ? 'POST':'PATCH';
-      final String url = (productId == null) ? '/products':'/products/$productId';
+      final String method = (productId == null) ? 'POST' : 'PATCH';
+      final String url = (productId == null) ? '/products' : '/products/$productId';
+
       productLike.remove('id');
+      productLike['images'] = await _uploadPhotos( productLike['images'] );
+
 
       final response = await dio.request(
         url,
@@ -38,42 +81,53 @@ class ProductsDatasourceImpl extends ProductsDatasource{
           method: method
         )
       );
+
       final product = ProductMaper.jsonToEntity(response.data);
       return product;
-    }catch(e) {
+
+    } catch (e) {
       throw Exception();
     }
-  }
 
-  @override
-  Future<List<Product>> getAllProducts({int limit = 10, int offset = 0}) async{
-    final response = await dio.get<List>('/products?limit=$limit&offset=$offset');
-    final List<Product> products = [];
-    for(var product in response.data ?? [] ){
-      products.add( ProductMaper.jsonToEntity(product) );
-    }
-    return products;
 
   }
 
   @override
-  Future<Product> getById(String id) async{
+  Future<Product> getById(String id) async {
+    
     try {
+      
       final response = await dio.get('/products/$id');
       final product = ProductMaper.jsonToEntity(response.data);
       return product;
-    }on DioException catch(e) {
-      if(e.response?.statusCode == 404) throw ProductNotFound();
+
+    } on DioException catch (e) {
+      if ( e.response!.statusCode == 404 ) throw ProductNotFound();
       throw Exception();
+
     }catch (e) {
       throw Exception();
     }
+
   }
 
+  @override
+  Future<List<Product>> getAllProducts({int limit = 10, int offset = 0}) async {
+    final response = await dio.get<List>('/products?limit=$limit&offset=$offset');
+    final List<Product> products = [];
+    for (final product in response.data ?? [] ) {
+      products.add(  ProductMaper.jsonToEntity(product)  );
+    }
+
+    return products;
+  }
+  
   @override
   Future<List<Product>> gsearchProductByTermn(String term) {
     // TODO: implement gsearchProductByTermn
     throw UnimplementedError();
   }
-
+  
+ 
+  
 }
